b8a51d37e3cc0e984dc607e6526c8b7d4723de93
awa784 <mawanimatul123@gmail.com>
2024-07-10 14:27:43 +0700

    Revert "Mengubah tampilan pada landing page"
    This reverts commit 49a7dda8e68c45cdaa68288c924a1481b002d415.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,21 +1,8 @@
-
-					<!--begin::Container-->
-					<div class="container">
-						<!--begin::Wrapper-->
-						<div class="d-flex flex-column flex-md-row flex-stack py-7 py-lg-10">
-							<!--begin::Copyright-->
-							<div class="d-flex align-items-center order-2 order-md-1">
-								<!--begin::Logo-->
-								<a href="#">
-									<img alt="Logo" src="img/logotext.png" class="h-50px h-md-50px" />
-								</a>
-								<!--end::Logo image-->
-								<!--begin::Logo image-->
-								<span class="mx-5 fs-6 fw-bold text-gray-600 pt-1" href="https://keenthemes.com">© 2024 <b>SiPiRang.</b> All rights reserved.</span>
-								<!--end::Logo image-->
-							</div>
-							<!--end::Copyright-->
-						</div>
-						<!--end::Wrapper-->
-					</div>
-					<!--end::Container-->
+ <!-- Start Footer -->
+ <footer class="footer d-flex justify-content-center">
+      <div class="container">
+        &copy; Copyright <strong><span>SiManuk</span></strong
+        >. All Rights Reserved
+      </div>
+    </footer>
+    <!-- End Footer -->
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,70 +1,47 @@
-					<!--begin::Header-->
-					<div class="landing-header" data-kt-sticky="true" data-kt-sticky-name="landing-header" data-kt-sticky-offset="{default: '200px', lg: '300px'}">
-						<!--begin::Container-->
-						<div class="container">
-							<!--begin::Wrapper-->
-							<div class="d-flex align-items-center justify-content-between">
-								<!--begin::Logo-->
-								<div class="d-flex align-items-center flex-equal">
-									<!--begin::Mobile menu toggle-->
-									<button class="btn btn-icon btn-active-color-primary me-3 d-flex d-lg-none" id="kt_landing_menu_toggle">
-										<!--begin::Svg Icon | path: icons/duotune/abstract/abs015.svg-->
-										<span class="svg-icon svg-icon-2hx">
-											<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-												<path d="M21 7H3C2.4 7 2 6.6 2 6V4C2 3.4 2.4 3 3 3H21C21.6 3 22 3.4 22 4V6C22 6.6 21.6 7 21 7Z" fill="black" />
-												<path opacity="0.3" d="M21 14H3C2.4 14 2 13.6 2 13V11C2 10.4 2.4 10 3 10H21C21.6 10 22 10.4 22 11V13C22 13.6 21.6 14 21 14ZM22 20V18C22 17.4 21.6 17 21 17H3C2.4 17 2 17.4 2 18V20C2 20.6 2.4 21 3 21H21C21.6 21 22 20.6 22 20Z" fill="black" />
-											</svg>
-										</span>
-										<!--end::Svg Icon-->
-									</button>
-									<!--end::Mobile menu toggle-->
-									<!--begin::Logo image-->
-									<a href="../../demo1/dist/landing.html">
-										<img alt="Logo" src="img/logotext.png" class="logo-default h-70px h-lg-70px" />
-										<img alt="Logo" src="img/logotext.png" class="logo-sticky h-70px h-lg-70px" />
-									</a>
-									<!--end::Logo image-->
-								</div>
-								<!--end::Logo-->
-								<!--begin::Menu wrapper-->
-								<div class="d-lg-block" id="kt_header_nav_wrapper">
-									<div class="d-lg-block p-5 p-lg-0" data-kt-drawer="true" data-kt-drawer-name="landing-menu" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="200px" data-kt-drawer-direction="start" data-kt-drawer-toggle="#kt_landing_menu_toggle" data-kt-swapper="true" data-kt-swapper-mode="prepend" data-kt-swapper-parent="{default: '/', lg: '#kt_header_nav_wrapper'}">
-										<!--begin::Menu-->
-										<div class="menu menu-column flex-nowrap menu-rounded menu-lg-row menu-title-gray-500 menu-state-title-primary nav nav-flush fs-5 fw-bold" id="kt_landing_menu">
-											<!--begin::Menu item-->
-											<div class="menu-item">
-												<!--begin::Menu link-->
-												<a class="menu-link nav-link active py-3 px-4 px-xxl-6" href="#top" data-kt-scroll-toggle="true" data-kt-drawer-dismiss="true">Home</a>
-												<!--end::Menu link-->
-											</div>
-											<!--end::Menu item-->
-											<!--begin::Menu item-->
-											<div class="menu-item">
-												<!--begin::Menu link-->
-												<a class="menu-link nav-link py-3 px-4 px-xxl-6" href="#bantuan" data-kt-scroll-toggle="true" data-kt-drawer-dismiss="true">Bantuan</a>
-												<!--end::Menu link-->
-											</div>
-											<!--end::Menu item-->
-											<!--begin::Menu item-->
-											<div class="menu-item">
-												<!--begin::Menu link-->
-												<a class="menu-link nav-link py-3 px-4 px-xxl-6" href="#tentang" data-kt-scroll-toggle="true" data-kt-drawer-dismiss="true">Tentang</a>
-												<!--end::Menu link-->
-											</div>
-											<!--end::Menu item-->
-										</div>
-										<!--end::Menu-->
-									</div>
-								</div>
-								<!--end::Menu wrapper-->
-								<!--begin::Toolbar-->
-								<div class="flex-equal text-end ms-1">
-									<a href="/login" class="btn btn-success">Login</a>
-								</div>
-								<!--end::Toolbar-->
-							</div>
-							<!--end::Wrapper-->
-						</div>
-						<!--end::Container-->
-					</div>
-					<!--end::Header-->
+<!-- Start Navbar -->
+<header class="header d-flex justify-content-center">
+    <div class="header_content d-flex flex-row align-items-center">
+      <a href="#">
+        <div class="logo_container">
+          <div class="logo">
+            <img src="img/logotext.png" alt="LOGO" />
+          </div>
+        </div>
+      </a>
+      <div class="main_nav_container">
+        <div class="main_nav">
+          <ul class="main_nav_list">
+            <li class="main_nav_item {{Request::is('') ? '' : 'active'}}">
+              <a href="/">Beranda</a>
+            </li>
+            <li class="main_nav_item {{Request::is('about') ? 'active' : ''}}">
+              <a href="/about">Tentang</a>
+            </li>
+            <li class="main_nav_item {{Request::is('help') ? 'active' : ''}}">
+              <a href="/help">Bantuan</a>
+            </li>
+            @auth
+            <li class="main_nav_item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Welcome back, {{auth()->user()->name}}
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="/dashboard/rooms"><i class="bi bi-layout-text-sidebar-reverse"></i> My Dashboard</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li>
+                  <form action="/logout" method="post">
+                      @csrf
+                      <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-right"></i> Logout</button>
+                  </form>  
+              </ul>
+            </li>
+            @else
+            <li class="main_nav_item {{Request::is('login') ? 'active' : ''}}">
+              <a href="/login">Login</a>
+            </li>
+            @endauth
+          </ul>
+        </div>
+      </div>
+  </header>
+  <!-- End Navbar -->
