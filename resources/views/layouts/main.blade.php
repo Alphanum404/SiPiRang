49a7dda8e68c45cdaa68288c924a1481b002d415
awa784 <mawanimatul123@gmail.com>
2024-07-10 13:46:21 +0700

    Mengubah tampilan pada landing page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,45 +1,319 @@
 <!DOCTYPE html>
+<!--
+Author: Keenthemes
+Product Name: Metronic - Bootstrap 5 HTML, VueJS, React, Angular & Laravel Admin Dashboard Theme
+Purchase: https://1.envato.market/EA4JP
+Website: http://www.keenthemes.com
+Contact: support@keenthemes.com
+Follow: www.twitter.com/keenthemes
+Dribbble: www.dribbble.com/keenthemes
+Like: www.facebook.com/keenthemes
+License: For each use you must have a valid license purchased only from above link in order to legally use the theme for your project.
+-->
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta
-      name="description"
-      content="Sistem Informasi Peminjaman Ruangan Kampus"
-    />
-    <meta name="keywords" content="Peminjaman Ruangan Kampus" />
-    <meta name="theme-color" content="#1A374D" />
-    <link rel="icon" href="/img/logo.png" />
-    <link rel="stylesheet" href="/css/index.css" />
-    <link rel="stylesheet" href="/css/responsive.css" />
-    <link rel="stylesheet" href="/css/normalize.css" />
-    <link rel="stylesheet" href="/css/landingPage.css" />
-    <link
-      rel="stylesheet"
-      href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
-      integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk"
-      crossorigin="anonymous"
-    />
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-      crossorigin="anonymous"
-    />
-    <title>SiManuk App | {{$title}} </title>
-  </head>
-  <body>
-      @include('partials.navbar')
-        
-      @yield('container')
+	<!--begin::Head-->
+	<head><base href="">
+  <title>SiManuk App | {{$title}} </title>
+  <meta name="description" content="The most advanced Bootstrap Admin Theme on Themeforest trusted by 94,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue &amp; Laravel versions. Grab your copy now and get life-time updates for free." />
+		<meta name="keywords" content="Metronic, bootstrap, bootstrap 5, Angular, VueJs, React, Laravel, admin themes, web design, figma, web development, free templates, free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta charset="utf-8" />
+		<meta property="og:locale" content="en_US" />
+		<meta property="og:type" content="article" />
+		<meta property="og:title" content="Metronic - Bootstrap 5 HTML, VueJS, React, Angular &amp; Laravel Admin Dashboard Theme" />
+		<meta property="og:url" content="https://keenthemes.com/metronic" />
+		<meta property="og:site_name" content="Keenthemes | Metronic" />
+		<link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
+		<link rel="shortcut icon" href="metro/assets/media/logos/favicon.ico" />
+		<!--begin::Fonts-->
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+		<!--end::Fonts-->
+		<!--begin::Global Stylesheets Bundle(used by all pages)-->
+		<link href="metro/assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
+		<link href="metro/assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+		<!--end::Global Stylesheets Bundle-->
+	</head>
+	<!--end::Head-->
+	<!--begin::Body-->
+	<body id="kt_body" data-bs-spy="scroll" data-bs-target="#kt_landing_menu" data-bs-offset="200" class="bg-white position-relative">
+		<!--begin::Main-->
+		<div class="d-flex flex-column flex-root">
+			<!--begin::Header Section-->
+			<div class="mb-0" id="home">
+				<!--begin::Wrapper-->
+				<div class="bgi-no-repeat bgi-size-contain bgi-position-x-center bgi-position-y-bottom landing-dark-bg" style="background-image: url(metro/assets/media/svg/illustrations/landing.svg)">
+					@include("partials.navbar")
 
-      @include('partials.footer')
-  <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-      crossorigin="anonymous"
-    ></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="/js/index.js"></script>
-  </body>
+					@yield("container")
+				</div>
+				<!--end::Wrapper-->
+				<!--begin::Curve bottom-->
+				<div class="landing-curve landing-dark-color mb-10 mb-lg-20">
+					<svg viewBox="15 12 1470 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M0 11C3.93573 11.3356 7.85984 11.6689 11.7725 12H1488.16C1492.1 11.6689 1496.04 11.3356 1500 11V12H1488.16C913.668 60.3476 586.282 60.6117 11.7725 12H0V11Z" fill="currentColor"></path>
+					</svg>
+				</div>
+				<!--end::Curve bottom-->
+			</div>
+			<!--end::Header Section-->
+			<!--begin::How It Works Section-->
+			<div class="mb-n10 mb-lg-n20 z-index-2">
+				<!--begin::Container-->
+				<div class="container">
+					<!--begin::Heading-->
+					<div class="text-center mb-17">
+						<!--begin::Title-->
+						<h3 class="fs-2hx text-dark mb-5" id="bantuan" data-kt-scroll-offset="{default: 100, lg: 150}">Tata Cara Peminjaman</h3>
+						<!--end::Title-->
+					</div>
+					<!--end::Heading-->
+					<!--begin::Product slider-->
+					<div class="tns tns-default">
+						<!--begin::Slider-->
+						<div data-tns="true" data-tns-loop="true" data-tns-swipe-angle="false" data-tns-speed="2000" data-tns-autoplay="true" data-tns-autoplay-timeout="18000" data-tns-controls="true" data-tns-nav="false" data-tns-items="1" data-tns-center="false" data-tns-dots="false" data-tns-prev-button="#kt_team_slider_prev1" data-tns-next-button="#kt_team_slider_next1">
+							<!--begin::Item-->
+							<div class="text-center px-5 pt-5 pt-lg-10 px-lg-10">
+								<img src="metro/assets/media/product-demos/demo1.png" class="card-rounded shadow mw-100" alt="" />
+							</div>
+							<!--end::Item-->
+						</div>
+						<!--end::Slider-->
+						<!--begin::Slider button-->
+						<button class="btn btn-icon btn-active-color-primary" id="kt_team_slider_prev1">
+							<!--begin::Svg Icon | path: icons/duotune/arrows/arr074.svg-->
+							<span class="svg-icon svg-icon-3x">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+									<path d="M11.2657 11.4343L15.45 7.25C15.8642 6.83579 15.8642 6.16421 15.45 5.75C15.0358 5.33579 14.3642 5.33579 13.95 5.75L8.40712 11.2929C8.01659 11.6834 8.01659 12.3166 8.40712 12.7071L13.95 18.25C14.3642 18.6642 15.0358 18.6642 15.45 18.25C15.8642 17.8358 15.8642 17.1642 15.45 16.75L11.2657 12.5657C10.9533 12.2533 10.9533 11.7467 11.2657 11.4343Z" fill="black" />
+								</svg>
+							</span>
+							<!--end::Svg Icon-->
+						</button>
+						<!--end::Slider button-->
+						<!--begin::Slider button-->
+						<button class="btn btn-icon btn-active-color-primary" id="kt_team_slider_next1">
+							<!--begin::Svg Icon | path: icons/duotune/arrows/arr071.svg-->
+							<span class="svg-icon svg-icon-3x">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+									<path d="M12.6343 12.5657L8.45001 16.75C8.0358 17.1642 8.0358 17.8358 8.45001 18.25C8.86423 18.6642 9.5358 18.6642 9.95001 18.25L15.4929 12.7071C15.8834 12.3166 15.8834 11.6834 15.4929 11.2929L9.95001 5.75C9.5358 5.33579 8.86423 5.33579 8.45001 5.75C8.0358 6.16421 8.0358 6.83579 8.45001 7.25L12.6343 11.4343C12.9467 11.7467 12.9467 12.2533 12.6343 12.5657Z" fill="black" />
+								</svg>
+							</span>
+							<!--end::Svg Icon-->
+						</button>
+						<!--end::Slider button-->
+					</div>
+					<!--end::Product slider-->
+				</div>
+				<!--end::Container-->
+			</div>
+			<!--end::How It Works Section-->
+			<!--begin::Statistics Section-->
+			<div class="mt-sm-n10">
+				<!--begin::Curve top-->
+				<div class="landing-curve landing-dark-color">
+					<svg viewBox="15 -1 1470 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M1 48C4.93573 47.6644 8.85984 47.3311 12.7725 47H1489.16C1493.1 47.3311 1497.04 47.6644 1501 48V47H1489.16C914.668 -1.34764 587.282 -1.61174 12.7725 47H1V48Z" fill="currentColor"></path>
+					</svg>
+				</div>
+				<!--end::Curve top-->
+				<!--begin::Wrapper-->
+				<div class="pb-15 pt-18 landing-dark-bg">
+					<!--begin::Container-->
+					<div class="container">
+						<!--begin::Heading-->
+						<div class="text-center mt-15 mb-18" id="tentang" data-kt-scroll-offset="{default: 100, lg: 150}">
+							<!--begin::Title-->
+							<h3 class="fs-2hx text-white fw-bolder mb-5">Tentang</h3>
+							<!--end::Title-->
+						</div>
+						<!--end::Heading-->
+						<!--begin::Testimonial-->
+						<div class="fs-2 fw-bold text-muted text-center mb-3">
+						<span class="fs-1 lh-1 text-gray-700">“</span>SiPiRang, Sistem Aplikasi Peminjaman Ruangan adalah inovasi digital yang dirancang untuk memudahkan mahasiswa dalam proses peminjaman ruangan di kampus secara online. Dengan implementasi sistem real-time, 
+						<span class="text-gray-700 me-1">SiPiRang</span>menawarkan solusi efisien dan cepat bagi mahasiswa yang membutuhkan ruangan untuk berbagai keperluan akademik maupun non-akademik. Aplikasi ini dirancang untuk mengoptimalkan penggunaan ruangan di kampus, memastikan proses peminjaman berjalan lancar, dan meningkatkan kepuasan pengguna.
+						<span class="fs-1 lh-1 text-gray-700">“</span></div>
+						<!--end::Testimonial-->
+					</div>
+					<!--end::Container-->
+				</div>
+				<!--end::Wrapper-->
+				<!--begin::Curve bottom-->
+				<div class="landing-curve landing-dark-color">
+					<svg viewBox="15 12 1470 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M0 11C3.93573 11.3356 7.85984 11.6689 11.7725 12H1488.16C1492.1 11.6689 1496.04 11.3356 1500 11V12H1488.16C913.668 60.3476 586.282 60.6117 11.7725 12H0V11Z" fill="currentColor"></path>
+					</svg>
+				</div>
+				<!--end::Curve bottom-->
+			</div>
+			<!--end::Statistics Section-->
+			<!--begin::Team Section-->
+			<div class="py-10 py-lg-20">
+				<!--begin::Container-->
+				<div class="container">
+					<!--begin::Heading-->
+					<div class="text-center mb-12">
+						<!--begin::Title-->
+						<h3 class="fs-2hx text-dark mb-5" id="team" data-kt-scroll-offset="{default: 100, lg: 150}">Team Kami</h3>
+						<!--end::Title-->
+					</div>
+					<!--end::Heading-->
+					<!--begin::Slider-->
+					<div class="tns tns-default">
+						<!--begin::Wrapper-->
+						<div data-tns="true" data-tns-loop="true" data-tns-swipe-angle="false" data-tns-speed="2000" data-tns-autoplay="true" data-tns-autoplay-timeout="18000" data-tns-controls="true" data-tns-nav="false" data-tns-items="1" data-tns-center="false" data-tns-dots="false" data-tns-prev-button="#kt_team_slider_prev" data-tns-next-button="#kt_team_slider_next" data-tns-responsive="{1200: {items: 3}, 992: {items: 2}}">
+							<!--begin::Item-->
+							<div class="text-center">
+								<!--begin::Photo-->
+								<div class="octagon mx-auto mb-5 d-flex w-200px h-200px bgi-no-repeat bgi-size-contain bgi-position-center" style="background-image:url('metro/assets/media/avatars/150-2.jpg')"></div>
+								<!--end::Photo-->
+								<!--begin::Person-->
+								<div class="mb-0">
+									<!--begin::Name-->
+									<a class="text-dark fw-bolder text-hover-primary fs-3">Aldi</a>
+									<!--end::Name-->
+									<!--begin::Position-->
+									<div class="text-muted fs-6 fw-bold mt-1">Full-Stack Developer</div>
+									<!--begin::Position-->
+								</div>
+								<!--end::Person-->
+							</div>
+							<!--end::Item-->
+							<!--begin::Item-->
+							<div class="text-center">
+								<!--begin::Photo-->
+								<div class="octagon mx-auto mb-5 d-flex w-200px h-200px bgi-no-repeat bgi-size-contain bgi-position-center" style="background-image:url('metro/assets/media/avatars/150-3.jpg')"></div>
+								<!--end::Photo-->
+								<!--begin::Person-->
+								<div class="mb-0">
+									<!--begin::Name-->
+									<a class="text-dark fw-bolder text-hover-primary fs-3">Ni'matul</a>
+									<!--end::Name-->
+									<!--begin::Position-->
+									<div class="text-muted fs-6 fw-bold mt-1">Project Manager</div>
+									<!--begin::Position-->
+								</div>
+								<!--end::Person-->
+							</div>
+							<!--end::Item-->
+							<!--begin::Item-->
+							<div class="text-center">
+								<!--begin::Photo-->
+								<div class="octagon mx-auto mb-5 d-flex w-200px h-200px bgi-no-repeat bgi-size-contain bgi-position-center" style="background-image:url('metro/assets/media/avatars/150-4.jpg')"></div>
+								<!--end::Photo-->
+								<!--begin::Person-->
+								<div class="mb-0">
+									<!--begin::Name-->
+									<a class="text-dark fw-bolder text-hover-primary fs-3">Ammar</a>
+									<!--end::Name-->
+									<!--begin::Position-->
+									<div class="text-muted fs-6 fw-bold mt-1">QA Manager</div>
+									<!--begin::Position-->
+								</div>
+								<!--end::Person-->
+							</div>
+							<!--end::Item-->
+						</div>
+						<!--end::Wrapper-->
+						<!--begin::Button-->
+						<button class="btn btn-icon btn-active-color-primary" id="kt_team_slider_prev">
+							<!--begin::Svg Icon | path: icons/duotune/arrows/arr074.svg-->
+							<span class="svg-icon svg-icon-3x">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+									<path d="M11.2657 11.4343L15.45 7.25C15.8642 6.83579 15.8642 6.16421 15.45 5.75C15.0358 5.33579 14.3642 5.33579 13.95 5.75L8.40712 11.2929C8.01659 11.6834 8.01659 12.3166 8.40712 12.7071L13.95 18.25C14.3642 18.6642 15.0358 18.6642 15.45 18.25C15.8642 17.8358 15.8642 17.1642 15.45 16.75L11.2657 12.5657C10.9533 12.2533 10.9533 11.7467 11.2657 11.4343Z" fill="black" />
+								</svg>
+							</span>
+							<!--end::Svg Icon-->
+						</button>
+						<!--end::Button-->
+						<!--begin::Button-->
+						<button class="btn btn-icon btn-active-color-primary" id="kt_team_slider_next">
+							<!--begin::Svg Icon | path: icons/duotune/arrows/arr071.svg-->
+							<span class="svg-icon svg-icon-3x">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+									<path d="M12.6343 12.5657L8.45001 16.75C8.0358 17.1642 8.0358 17.8358 8.45001 18.25C8.86423 18.6642 9.5358 18.6642 9.95001 18.25L15.4929 12.7071C15.8834 12.3166 15.8834 11.6834 15.4929 11.2929L9.95001 5.75C9.5358 5.33579 8.86423 5.33579 8.45001 5.75C8.0358 6.16421 8.0358 6.83579 8.45001 7.25L12.6343 11.4343C12.9467 11.7467 12.9467 12.2533 12.6343 12.5657Z" fill="black" />
+								</svg>
+							</span>
+							<!--end::Svg Icon-->
+						</button>
+						<!--end::Button-->
+					</div>
+					<!--end::Slider-->
+				</div>
+				<!--end::Container-->
+			</div>
+			<!--end::Team Section-->
+			<!--begin::Footer Section-->
+			<div class="mb-0">
+				<!--begin::Curve top-->
+				<div class="landing-curve landing-dark-color">
+					<svg viewBox="15 -1 1470 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M1 48C4.93573 47.6644 8.85984 47.3311 12.7725 47H1489.16C1493.1 47.3311 1497.04 47.6644 1501 48V47H1489.16C914.668 -1.34764 587.282 -1.61174 12.7725 47H1V48Z" fill="currentColor"></path>
+					</svg>
+				</div>
+				<!--end::Curve top-->
+				<!--begin::Wrapper-->
+				<div class="landing-dark-bg pt-20">
+					<!--begin::Container-->
+					<div class="container">
+						<!--begin::Row-->
+						<div class="row py-10 py-lg-20">
+							<!--begin::Col-->
+							<div class="col-lg-6 pe-lg-16 mb-10 mb-lg-0">
+							</div>
+							<!--end::Col-->
+              <div class="d-flex justify-content-center">
+									<!--begin::Links-->
+									<div class="d-flex fw-bold flex-column me-20">
+										<!--begin::Subtitle-->
+										<h4 class="fw-bolder text-gray-400 mb-6">Lainnya dari SiPiRang</h4>
+										<!--end::Subtitle-->
+										<!--begin::Link-->
+										<a href="//github.com/Alphanum404/SiPiRang" target="_blank" class="mb-6">
+											<img src="metro/assets/media/svg/brand-logos/github.svg" class="h-20px me-2" alt="" />
+											<span class="text-white opacity-50 text-hover-primary fs-5 mb-6">Github</span>
+										</a>
+										<!--end::Link-->
+									</div>
+</>
+						</div>
+						<!--end::Row-->
+					</div>
+					<!--end::Container-->
+					<!--begin::Separator-->
+					<div class="landing-dark-separator"></div>
+					<!--end::Separator-->
+					@include("partials.footer")
+				</div>
+				<!--end::Wrapper-->
+			</div>
+			<!--end::Footer Section-->
+			<!--begin::Scrolltop-->
+			<div id="kt_scrolltop" class="scrolltop" data-kt-scrolltop="true">
+				<!--begin::Svg Icon | path: icons/duotune/arrows/arr066.svg-->
+				<span class="svg-icon">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+						<rect opacity="0.5" x="13" y="6" width="13" height="2" rx="1" transform="rotate(90 13 6)" fill="black" />
+						<path d="M12.5657 8.56569L16.75 12.75C17.1642 13.1642 17.8358 13.1642 18.25 12.75C18.6642 12.3358 18.6642 11.6642 18.25 11.25L12.7071 5.70711C12.3166 5.31658 11.6834 5.31658 11.2929 5.70711L5.75 11.25C5.33579 11.6642 5.33579 12.3358 5.75 12.75C6.16421 13.1642 6.83579 13.1642 7.25 12.75L11.4343 8.56569C11.7467 8.25327 12.2533 8.25327 12.5657 8.56569Z" fill="black" />
+					</svg>
+				</span>
+				<!--end::Svg Icon-->
+			</div>
+			<!--end::Scrolltop-->
+		</div>
+		<!--end::Main-->
+		<script>var hostUrl = "metro/assets/";</script>
+		<!--begin::Javascript-->
+		<!--begin::Global Javascript Bundle(used by all pages)-->
+		<script src="metro/assets/plugins/global/plugins.bundle.js"></script>
+		<script src="metro/assets/js/scripts.bundle.js"></script>
+		<!--end::Global Javascript Bundle-->
+		<!--begin::Page Vendors Javascript(used by this page)-->
+		<script src="metro/assets/plugins/custom/fslightbox/fslightbox.bundle.js"></script>
+		<script src="metro/assets/plugins/custom/typedjs/typedjs.bundle.js"></script>
+		<!--end::Page Vendors Javascript-->
+		<!--begin::Page Custom Javascript(used by this page)-->
+		<script src="metro/assets/js/custom/landing.js"></script>
+		<!--end::Page Custom Javascript-->
+		<!--end::Javascript-->
+	</body>
+	<!--end::Body-->
 </html>
