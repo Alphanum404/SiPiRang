3d22bfedb70e761d8a9cc64b734c45cca7f9275b
Alphanum404 <aldiem2004@gmail.com>
2024-07-14 11:59:03 +0700

    chore: change template of admin.index

--- resources/views/dashboard/admin/index.blade.php
+++ resources/views/dashboard/admin/index.blade.php
@@ -1,45 +1,150 @@
-@extends('dashboard.layouts.main')
-
 @section('container')
-<div class="col-md-10 p-0">
-    <h2 class="content-title text-center">Daftar {{$title}}</h2>
-<div class="card-body text-end">
-  <a href="/dashboard/users" type="button" class="mb-3 btn button btn-primary">
-    Pilih dari User
-  </a>
-  <button type="button" class="mb-3 btn button btn-primary" data-bs-toggle="modal" data-bs-target="#addAdmin">
-    Tambah Data Baru
-  </button>
-  <div class="table-responsive">
-    <table class="table table-hover table-stripped table-bordered text-center dt-head-center" id="datatable">
-      <thead class="table-info">
-        <tr>
-          <th scope="row">No.</th>
-          <th scope="row">Username</th>
-          <th scope="row">Nomor Induk</th>
-          <th scope="row">Email</th>
-          <th scope="row">Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach($admins as $admin) 
-        <tr>
-          <th scope="row">{{$loop->iteration}} </th>
-          <td>{{$admin->name}} </td>
-          <td>{{$admin->nomor_induk}} </td>
-          <td>{{$admin->email}} </td>
-          <td style="font-size: 22px;">
-            <a href="/dashboard/users/{{ $admin->id }}/edit" class="edituser" id="edituser" data-id="{{ $admin->id }}" data-bs-toggle="modal" data-bs-target="#edituser"><i class="bi bi-pencil-square text-warning"></i></a>&nbsp;
-            <a href="/dashboard/admin/{{ $admin->id }}/removeAdmin" class="bi bi-trash-fill text-danger border-0" onclick="return confirm('Ubah admin menjadi mahasiswa?')"></a>
-          </td>
-        </tr>
-        @endforeach
-      </tbody>
-    </table>
+<!-- begin::Post -->
+<div class="post d-flex flex-column-fluid" id="kt_post">
+  <!--begin::Container-->
+  <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+    <div id="kt_content_container" class="container-xxl">
+      <!--begin::Tables Widget 11-->
+      <div class="card mb-5 mb-xl-8">
+        <div class="card card-xl-stretch mb-xl-8">
+          <!--begin::Header-->
+          <div class="card-header border-0 pt-5">
+            <h3 class="card-title align-items-start flex-column">
+              <span class="card-label fw-bolder fs-3 mb-1">List {{$title}}</span>
+            </h3>
+            <div class="card-toolbar">
+              <input type="text" id="myInput" class="form-control" style="width: 100px; height: 37px" placeholder="Search" onkeyup="filterTable()" />
+              <a href="/dashboard/users" class="btn btn-sm btn-light-primary">
+                <!--begin::Svg Icon | path: icons/duotune/arrows/arr075.svg-->
+                <span class="svg-icon svg-icon-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="800px" height="800px" viewBox="0 0 24 24" fill="none">
+                    <path d="M12 3C4.5885 3 3 4.5885 3 12C3 19.4115 4.5885 21 12 21C19.4115 21 21 19.4115 21 12" stroke="#323232" stroke-width="2" stroke-linecap="round" />
+                    <path d="M20.5 3.5L15 9" stroke="#323232" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M16 3H20.6717C20.853 3 21 3.14703 21 3.32837V8" stroke="#323232" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+                </span>
+                <!--end::Svg Icon-->Select From User</a>
+              <button class="btn btn-sm btn-light-danger" data-bs-toggle="modal" data-bs-target="#addAdmin">
+                <!--begin::Svg Icon | path: icons/duotune/arrows/arr075.svg-->
+                <span class="svg-icon svg-icon-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                    <rect opacity="0.5" x="11.364" y="20.364" width="16" height="2" rx="1" transform="rotate(-90 11.364 20.364)" fill="red"></rect>
+                    <rect x="4.36396" y="11.364" width="16" height="2" rx="1" fill="red"></rect>
+                  </svg>
+                </span>
+                <!--end::Svg Icon-->Add Data
+              </button>
+            </div>
+          </div>
+          <!--end::Header-->
+          <!--begin::Body-->
+          <div class="card-body py-3">
+            <!--begin::Table container-->
+            <div class="table-responsive">
+              <!--begin::Table-->
+              <table class="table align-middle gs-0 gy-4" id="myTable">
+                <!--begin::Table head-->
+                <thead>
+                  <tr class="fw-bolder text-muted bg-light">
+                    <th scope="row" class="ps-4 min-w-25px rounded-start">No</th>
+                    <th scope="row" class="min-w-125px">Username</th>
+                    <th scope="row" class="min-w-125px">Nomor Induk</th>
+                    <th scope="row" class="min-w-200px">Email</th>
+                    <th scope="row" class="min-w-150px">Role</th>
+                    <th scope="row" class="min-w-100px text-end rounded-end"></th>
+                  </tr>
+                </thead>
+                <!--end::Table head-->
+                <!--begin::Table body-->
+                <tbody>
+                  @foreach($admins as $admin)
+                  <tr id="data">
+                    <td>
+                      <div class="d-flex align-items-center">
+                        <div class="symbol symbol-50px me-5"></div>
+                        <div class="d-flex justify-content-start flex-column">
+                          <a class="text-dark fw-bolder text-hover-primary mb-1 fs-6">{{$loop->iteration}}</a>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      <a class="text-dark fw-bolder text-hover-primary d-block mb-1 fs-6">{{$admin->name}}</a>
+                    </td>
+                    <td>
+                      <a class="text-dark fw-bolder text-hover-primary d-block mb-1 fs-6">{{$admin->nomor_induk}}</a>
+                    </td>
+                    <td>
+                      <a class="text-dark fw-aolder text-hover-primary d-block mb-1 fs-6">{{$admin->email}}</a>
+                    </td>
+                    <td>
+                      <span class="badge badge-light-warning fs-7 fw-bold">Admin</span>
+                    </td>
+                    <td class="text-end">
+                      <a href="/dashboard/users/{{ $admin->id }}/edit" class="edituser btn-icon btn-bg-light btn-active-color-primary btn-sm me-1" id="edituser" name="edituser" data-id="{{ $admin->id }}" data-bs-toggle="modal" data-bs-target="#edituser">
+                        <!--begin::Svg Icon | path: icons/duotune/art/art005.svg-->
+                        <span class="svg-icon svg-icon-3">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <path opacity="0.3" d="M21.4 8.35303L19.241 10.511L13.485 4.755L15.643 2.59595C16.0248 2.21423 16.5426 1.99988 17.0825 1.99988C17.6224 1.99988 18.1402 2.21423 18.522 2.59595L21.4 5.474C21.7817 5.85581 21.9962 6.37355 21.9962 6.91345C21.9962 7.45335 21.7817 7.97122 21.4 8.35303ZM3.68699 21.932L9.88699 19.865L4.13099 14.109L2.06399 20.309C1.98815 20.5354 1.97703 20.7787 2.03189 21.0111C2.08674 21.2436 2.2054 21.4561 2.37449 21.6248C2.54359 21.7934 2.75641 21.9115 2.989 21.9658C3.22158 22.0201 3.4647 22.0084 3.69099 21.932H3.68699Z" fill="black"></path>
+                            <path d="M5.574 21.3L3.692 21.928C3.46591 22.0032 3.22334 22.0141 2.99144 21.9594C2.75954 21.9046 2.54744 21.7864 2.3789 21.6179C2.21036 21.4495 2.09202 21.2375 2.03711 21.0056C1.9822 20.7737 1.99289 20.5312 2.06799 20.3051L2.696 18.422L5.574 21.3ZM4.13499 14.105L9.891 19.861L19.245 10.507L13.489 4.75098L4.13499 14.105Z" fill="black"></path>
+                          </svg>
+                        </span>
+                        <!--end::Svg Icon-->
+                      </a>
+                      <a href="/dashboard/admin/{{ $admin->id }}/removeAdmin" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm" onclick="return confirm('Ubah admin menjadi mahasiswa?')">
+                        <!--begin::Svg Icon | path: icons/duotune/general/gen027.svg-->
+                        <span class="svg-icon svg-icon-3">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <path d="M5 9C5 8.44772 5.44772 8 6 8H18C18.5523 8 19 8.44772 19 9V18C19 19.6569 17.6569 21 16 21H8C6.34315 21 5 19.6569 5 18V9Z" fill="black"></path>
+                            <path opacity="0.5" d="M5 5C5 4.44772 5.44772 4 6 4H18C18.5523 4 19 4.44772 19 5V5C19 5.55228 18.5523 6 18 6H6C5.44772 6 5 5.55228 5 5V5Z" fill="black"></path>
+                            <path opacity="0.5" d="M9 4C9 3.44772 9.44772 3 10 3H14C14.5523 3 15 3.44772 15 4V4H9V4Z" fill="black"></path>
+                          </svg>
+                        </span>
+                        <!--end::Svg Icon-->
+                      </a>
+                    </td>
+                  </tr>
+                  @endforeach
+                </tbody>
+                <!--end::Table body-->
+              </table>
+              <!--end::Table-->
+            </div>
+            <!--begin::Pagination-->
+            <!-- <div class="d-flex justify-content-end">
+              <nav aria-label="Page navigation">
+                <ul class="pagination">
+                  <li class="page-item disabled">
+                    <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                  </li>
+                  <li class="page-item active" aria-current="page">
+                    <a class="page-link" href="#">1</a>
+                  </li>
+                  <li class="page-item">
+                    <a class="page-link" href="#">2</a>
+                  </li>
+                  <li class="page-item">
+                    <a class="page-link" href="#">3</a>
+                  </li>
+                  <li class="page-item">
+                    <a class="page-link" href="#">Next</a>
+                  </li>
+                </ul>
+              </nav>
+            </div> -->
+            <!--end::Pagination-->
+            <!--end::Table container-->
+          </div>
+          <!--begin::Body-->
+        </div>
+        <!--end::Tables Widget 11-->
+      </div>
+    </div>
+    <!--end::Container-->
   </div>
 </div>
-</div>
+<!-- end::Post -->
 @extends('dashboard.partials.addAdminModal')
 @extends('dashboard.partials.editUserModal')
 {{-- @extends('dashboard.partials.chooseAdminModal') --}}
 @endsection
+@endif
