aa552b22577842594904689e71c70a0dc8983fac
awa784 <mawanimatul123@gmail.com>
2024-07-18 08:20:38 +0700

    mengupdate halaman setting

--- resources/views/dashboard/users/setting.blade.php
+++ resources/views/dashboard/users/setting.blade.php
@@ -7,15 +7,25 @@
     <div id="kt_content_container" class="container-xxl">
         <!--begin::Tables Widget 11-->
         @if(session()->has('passwordSuccess'))
-        <div class="col-md-16 mx-auto alert alert-success text-center alert-success alert-dismissible fade show" style="margin-top: 50px" role="alert">
-            {{session('passwordSuccess')}}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
+            <div class="col-md-16 mx-auto alert alert-success text-center alert-success alert-dismissible fade show"
+                style="margin-top: 50px" role="alert">
+                {{session('passwordSuccess')}}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
+        @if(session()->has('passwordError'))
+            <div class="col-md-16 mx-auto alert alert-danger text-center alert-success alert-dismissible fade show"
+                style="margin-top: 50px" role="alert">
+                {{session('passwordError')}}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
         @endif
         <!-- begin::Card -->
         <div class="card mb-5 mb-xl-10">
             <!--begin::Card header-->
-            <div class="card-header border-0 cursor-pointer" role="button" data-bs-toggle="collapse" data-bs-target="#kt_account_profile_details" aria-expanded="true" aria-controls="kt_account_profile_details">
+            <div class="card-header border-0 cursor-pointer" role="button" data-bs-toggle="collapse"
+                data-bs-target="#kt_account_profile_details" aria-expanded="true"
+                aria-controls="kt_account_profile_details">
                 <!--begin::Card title-->
                 <div class="card-title m-0">
                     <h3 class="fw-bolder m-0">Profile Details</h3>
@@ -26,7 +36,9 @@
             <!--begin::Content-->
             <div id="kt_account_profile_details" class="collapse show">
                 <!--begin::Form-->
-                <form method="post" id="kt_account_profile_details_form" class="form fv-plugins-bootstrap5 fv-plugins-framework" novalidate="novalidate">
+                <form method="post" action="{{ route('users.update') }}" id="kt_account_profile_details_form"
+                    class="form fv-plugins-bootstrap5 fv-plugins-framework" novalidate="novalidate">
+                    @csrf
                     <!--begin::Card body-->
                     <div class="card-body border-top p-9">
                         <!--begin::Input group-->
@@ -36,7 +48,8 @@
                             <!--end::Label-->
                             <!--begin::Col-->
                             <div class="col-lg-8 fv-row fv-plugins-icon-container">
-                                <input type="text" name="name" class="form-control form-control-lg form-control-solid" value="{{ auth()->user()->name }}" readonly>
+                                <input type="text" name="name" class="form-control form-control-lg form-control-solid"
+                                    value="{{ auth()->user()->name }}" readonly>
                                 <div class="fv-plugins-message-container invalid-feedback"></div>
                             </div>
                             <!--end::Col-->
@@ -49,7 +62,78 @@
                             <!--end::Label-->
                             <!--begin::Col-->
                             <div class="col-lg-8 fv-row">
-                                <input type="text" name="nomor_induk" class="form-control form-control-lg form-control-solid" value="{{ auth()->user()->nomor_induk }}" readonly>
+                                <input type="text" name="nomor_induk"
+                                    class="form-control form-control-lg form-control-solid"
+                                    value="{{ auth()->user()->nomor_induk }}" readonly>
+                            </div>
+                            <!--end::Col-->
+                        </div>
+                        <!--end::Input group-->
+                        <!--begin::Input group-->
+                        <div class="row mb-6">
+                            <!--begin::Label-->
+                            <label class="col-lg-4 col-form-label fw-bold fs-6">Email</label>
+                            <!--end::Label-->
+                            <!--begin::Col-->
+                            <div class="col-lg-8 fv-row">
+                                <input type="text" name="email" class="form-control form-control-lg form-control-solid"
+                                    value="{{ auth()->user()->email }}" readonly>
+                            </div>
+                            <!--end::Col-->
+                        </div>
+                        <!--end::Input group-->
+                        <div class="row mb-6">
+                            <!--begin::Label-->
+                            <label class="col-lg-4 col-form-label fw-bold fs-6">Role</label>
+                            <!--end::Label-->
+                            <!--begin::Col-->
+                            <div class="col-lg-8 fv-row">
+                                @if(auth()->user()->role_id == 1)
+                                    <input type="text" name="role" class="form-control form-control-lg form-control-solid"
+                                        value="SUPER ADMIN" readonly>
+                                @elseif(auth()->user()->role_id == 2)
+                                    <input type="text" name="role" class="form-control form-control-lg form-control-solid"
+                                        value="ADMIN" readonly>
+                                @elseif(auth()->user()->role_id == 3)
+                                    <input type="text" name="role" class="form-control form-control-lg form-control-solid"
+                                        value="DOSEN" readonly>
+                                @elseif(auth()->user()->role_id == 4)
+                                    <input type="text" name="role" class="form-control form-control-lg form-control-solid"
+                                        value="KETUA KELAS" readonly>
+                                @elseif(auth()->user()->role_id == 5)
+                                    <input type="text" name="role" class="form-control form-control-lg form-control-solid"
+                                        value="MAHASISWA" readonly>
+                                @endif
+
+                            </div>
+                            <!--end::Col-->
+                        </div>
+                        <!--end::Input group-->
+                        <!--begin::Input group-->
+                        <div class="row mb-6">
+                            <!--begin::Label-->
+                            <label class="col-lg-4 col-form-label required fw-bold fs-6">Old Password</label>
+                            <!--end::Label-->
+                            <!--begin::Col-->
+                            <div class="col-lg-8 fv-row">
+                                <input type="password" name="old_password"
+                                    class="form-control form-control-lg form-control-solid"
+                                    placeholder="type old password here" required>
+                            </div>
+                            <!--end::Col-->
+                        </div>
+                        <!--end::Input group-->
+                        
+                        <!--begin::Input group-->
+                        <div class="row mb-6">
+                            <!--begin::Label-->
+                            <label class="col-lg-4 col-form-label required fw-bold fs-6">New Password</label>
+                            <!--end::Label-->
+                            <!--begin::Col-->
+                            <div class="col-lg-8 fv-row">
+                                <input type="password" name="new_password"
+                                    class="form-control form-control-lg form-control-solid"
+                                    placeholder="type new password" required>
                             </div>
                             <!--end::Col-->
                         </div>
@@ -58,11 +142,10 @@
                     <!--end::Card body-->
                     <!--begin::Actions-->
                     <div class="card-footer d-flex justify-content-end py-6 px-9">
-                        <button type="submit" class="btn btn-primary" id="kt_account_profile_details_submit">Save Changes</button>
+                        <button name="submit" type="submit" class="btn btn-primary"
+                            id="kt_account_profile_details_submit">Save Changes</button>
                     </div>
                     <!--end::Actions-->
-                    <input type="hidden">
-                    <div></div>
                 </form>
                 <!--end::Form-->
             </div>
